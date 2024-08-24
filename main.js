287da826f20d984752bed8c20ef310cf34e997d4
AndreaHebeGonzalez <andreagzlzhebe2907@gmail.com>
2024-08-24 20:46:45 -0300

    practica gsap - 1

---
+++ main.js
@@ -0,0 +1,18 @@
+const tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '#parallax', 
+        start: 'top top',
+        end: 'bottom top',
+        scrub: true, 
+        markers: true,
+    }
+});
+
+gsap.utils.toArray('.layer').forEach(layer => {
+    const speed = layer.dataset.speed; //accedo al valor del atributo data-speed
+    const movement = -(layer.offsetHeight * speed) //altura total del elemento c-p-b
+    tl.to(layer, {
+        y: movement, 
+        ease: "none"
+    }, 0) //tiempo de inicio de la animacion en la linea de tiempo
+});
